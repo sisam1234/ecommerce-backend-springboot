83e046c0f3937eedbfc4eb040abe2b8fbe7501fe
sisam1234 <sisamghaju72@gmail.com>
2026-02-26 19:29:33 +0545

    enable CROS

--- src/main/java/com/example/restapis/config/WebConfig.java
+++ src/main/java/com/example/restapis/config/WebConfig.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -17,5 +17,13 @@ public class WebConfig implements WebMvcConfigurer{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry){
         registry.addResourceHandler("/product/images/**").addResourceLocations("file:"+path+File.separator);
+    }
+     @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") 
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
